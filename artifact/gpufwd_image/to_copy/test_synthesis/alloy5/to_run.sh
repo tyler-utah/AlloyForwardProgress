79b5e44dbb8abfc154031bda315b1014374896c4
Tyler Sorensen <tyler.sorensen@outlook.com>
2021-07-13 00:42:19 -0700

    updated to run script

--- artifact/gpufwd_image/to_copy/test_synthesis/alloy5/to_run.sh
+++ artifact/gpufwd_image/to_copy/test_synthesis/alloy5/to_run.sh
@@ -2,7 +2,7 @@
 # Only the 2 threaded command is uncommented because the others take too long to run
 
 bash run_config.sh "2t_2i" "run to_run for 2 V,   2 Thread,  7 S,  8 A, 5 L_state, 4 G_memory, 2 X,  2 Instruction"
-#bash run_config.sh "2t_3i" "run to_run for 2 V,   2 Thread,  7 S,  8 A, 5 L_state, 4 G_memory, 2 X,  2 Instruction"
-#bash run_config.sh "2t_4i" "run to_run for 2 V,   2 Thread,  7 S,  8 A, 5 L_state, 4 G_memory, 2 X,  2 Instruction"
-#bash run_config.sh "3t_3i" "run to_run for 2 V,   2 Thread,  7 S,  8 A, 5 L_state, 4 G_memory, 2 X,  2 Instruction"
-#bash run_config.sh "3t_4i" "run to_run for 2 V,   2 Thread,  7 S,  8 A, 5 L_state, 4 G_memory, 2 X,  2 Instruction"
+#bash run_config.sh "2t_3i" "run to_run for 2 V,   2 Thread,  12 S,  14 A, 12 L_state, 4 G_memory, 2 X,  3 Instruction"
+#bash run_config.sh "2t_4i" "run to_run for 2 V,   2 Thread,  16 S,  24 A, 16 L_state, 4 G_memory, 2 X,  4 Instruction"
+#bash run_config.sh "3t_3i" "run to_run for 2 V,   3 Thread,  16 S,  24 A, 16 L_state, 4 G_memory, 2 X,  3 Instruction"
+#bash run_config.sh "3t_4i" "run to_run for 2 V,   3 Thread,  16 S,  24 A, 16 L_state, 4 G_memory, 2 X,  4 Instruction"
